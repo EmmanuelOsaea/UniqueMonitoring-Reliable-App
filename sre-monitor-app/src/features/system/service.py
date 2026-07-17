42a22549e1d033c6182041b0a975d17be0c8f22e
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-17 23:00:22 +0100

    Correct IP address retrieval in get_system_location
    Fix variable name and improve function return structure.

--- sre-monitor-app/src/features/system/service.py
+++ sre-monitor-app/src/features/system/service.py
@@ -6,10 +6,11 @@ class SystemService:
      def get_system_location() -> dict:
        "Fetches the  current local host name and Ip address. """
        hostname = socket.gethostname(hostname)
-       
+       ip address = socket.gethostname(hostname)
+     return {"hostname": hostname, "ip_address": ip address}
 
+@staticmethod
 
-
-
-
+"memory_usage_percent": 
+"capacity_status": status
 }
