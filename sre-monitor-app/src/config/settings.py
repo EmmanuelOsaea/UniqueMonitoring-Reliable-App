f421c0283c037bfc7acd3a76e759f197038c06d7
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-17 21:16:37 +0100

    Implement settings class for application configuration
    Add settings configuration for Azure client and tenant IDs.

--- sre-monitor-app/src/config/settings.py
+++ sre-monitor-app/src/config/settings.py
@@ -1,1 +1,16 @@
+import os
+from pydantic_settings import
+BaseSettings
 
+class Settings(BaseSettings):
+ APP_NAME:str = "SRE Monitor Detector"
+ AZURE_CLIENT_ID: str = 
+ os.getenv("AZURE_CLIENT_ID", "")
+
+AZURE_TENANT_ID: str =
+os.getenv("AZURE_TENANT_ID", "")
+
+class Config
+env_file = ".env"
+        
+settings = Settings()
