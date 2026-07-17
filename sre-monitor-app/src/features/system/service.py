0292069610ee1f8fc1720081a3b03e5a98ffed13
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-07-17 22:46:11 +0100

    Update service.py

--- sre-monitor-app/src/features/system/service.py
+++ sre-monitor-app/src/features/system/service.py
@@ -1,1 +1,15 @@
+import socket
+import psutil
 
+class SystemService:
+     @staticmethod
+     def get_system_location() -> dict:
+       "Fetches the  current local host name and Ip address. """
+       hostname = socket.gethostname(hostname)
+       
+
+
+
+
+
+}
