1cc1711e16067a0fa12a0267ef03ddab07d6472f
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-09 23:50:41 +0100

    Update app_api.py

--- sre-monitor-app/monitor-python/venv/app_api.py
+++ sre-monitor-app/monitor-python/venv/app_api.py
@@ -7,3 +7,14 @@ app = FastAPI()
 
 @app.get("/metrics")
 def read_metrics():
+  cpu = psutil.cpu_percent(interval=0.1)
+  memory = psutil.virtual_memory.percent
+
+  return {
+    status: "healthy",
+    cpu_usage: cpu,
+    memory_usage: memory
+  }
+
+if __name__ == __"main"__:
+uvicorn.run(app, host="12.0.0.1", port=8000)
