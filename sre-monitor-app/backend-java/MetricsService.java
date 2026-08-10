e8b1e5895ff63d8c29684787ad307aa8001a5abf
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-10 22:27:06 +0100

    Add MetricsService class for metrics handling

---
+++ sre-monitor-app/backend-java/MetricsService.java
@@ -0,0 +1,14 @@
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+ public class MetricsService {
+
+private final RestClient restClient = RestClient.create();
+
+
+    url ("http
+   .retrieve()
+   .body(String.Class);
+ }
+ }
