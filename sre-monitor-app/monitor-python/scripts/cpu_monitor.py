bb63cb7fa9c1a2c51ee7501412459b3cbdb273e6
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-11 01:23:50 +0100

    Update cpu_monitor.py

--- sre-monitor-app/monitor-python/scripts/cpu_monitor.py
+++ sre-monitor-app/monitor-python/scripts/cpu_monitor.py
@@ -1,1 +1,33 @@
+import psutil
 
+def get_cpu_metrics():
+  """
+  Gathers comprehensive Cpu performance metrics
+  """
+  try: 
+  cpu_utilization = psutil.cpu_percent(interval=1)
+  per_cpu_utilization = psutil.cpu_percent(interval=None, percpu=True)
+  cpu_count_logical = psutil.cpu_count()
+  cpu_count_physical = psutil.cpu_count(logical=False)
+
+cpu_freq = psutil.cpu_freq()
+current_freq = cpu_freq.current if cpu_freq else 0
+
+return {
+  "status": "success",
+   "metrics": {
+   "total_utilization_percent" = cpu_utilization,
+   "per_core_utilization_percent" = per_cpu_utilization,
+   "logical_cores" = cpu_count_logical,
+   "physical_cores" = cpu_count_physical,
+   "current_freq_mhz" = current_freq
+}
+}
+except Exception as e:
+      return {
+        "status": "error"
+        "message": f"Failed to fetch CPU metrics: {str(e)}"
+}
+if __name__ == "__main__":
+     print ("Testing cpu monitor...")
+print (get_cpu_metrics())
