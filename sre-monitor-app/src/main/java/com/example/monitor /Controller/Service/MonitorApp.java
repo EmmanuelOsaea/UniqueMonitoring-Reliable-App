c79cdd7514d87eb3e519ff372ffe974064c66e5f
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-12 05:50:21 +0100

    Update MonitorApp.java

--- sre-monitor-app/src/main/java/com/example/monitor /Controller/Service/MonitorApp.java
+++ sre-monitor-app/src/main/java/com/example/monitor /Controller/Service/MonitorApp.java
@@ -1,1 +1,24 @@
+package com.example
+  import java.io.InputStream
+  import java.util.properties
 
+    public class MonitorApp {
+        public static void main(String[]args) {
+          MonitorApp app= new MonitorApp();
+          app.loadConfig(),
+           }
+
+public void loadConfig() {
+  try input stream input = get class getClass loader getResourceAsStream("config.properties")) {
+    if (input == null {
+      System.out.printIn("Sorry, unable to find config.properties");
+      return;
+    }
+ Properties prop= new Properties();
+    prop.load(input);
+    System.out.printIn("App Name: " + prop.getProperty("app.name"));
+    } catch (Exception ex) {
+         ex.printStackTrace();
+    }
+    }
+    }
