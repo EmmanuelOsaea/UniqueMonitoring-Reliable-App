554d0e4d9f71af2f379f26b0465912840b9617b4
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-12 04:28:01 +0100

    Correct psutil import and update metric collection
    Fix import typo and improve logging for metrics collection.

--- sre-monitor-app/system/router.py/service.py
+++ sre-monitor-app/system/router.py/service.py
@@ -1,1 +1,35 @@
+import logging
+import time
+try: 
+   impprt psutil
+except ImportError:
+psutil = None
 
+
+logging.basicConfig(level=logging.INFO, format ='%(asctime)s - %(levelname)s - %(message)s')
+
+class SystemMonitorService
+def __init__(self, interval=5):
+self interval = interval 
+
+def collect_metrics(self):
+if not psutil: 
+logging.error("psutil is not installed. Run 'pip install psutil'.")
+return{}
+
+return {
+"cpu percent": psutil cpu percent ps(interval=1)
+"memory_percent": psutil virtual_memory().percent,
+"disk percent": psutil disk usage('/').percent
+
+def run(self):
+logging.info("Starting System Monitor Service..")
+while True
+metrics = self.collect_metrics()
+if metrics:
+logging.info(f"Collected Metrics: {metrics}")
+time.sleep(self.interval)
+
+if __name__ == "__main__":
+service = SystemMonitorService()
+service.run()
