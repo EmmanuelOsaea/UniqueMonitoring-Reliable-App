ec5ead7729fb19747e798630961ef6c1f7b45a14
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-12 06:30:42 +0100

    Update variables.tf

--- sre-monitor-app/infra/variables.tf
+++ sre-monitor-app/infra/variables.tf
@@ -1,1 +1,25 @@
+# 1. Environment & Project Metadat
+var "project_name" {
+type = string
+description = "The prefix used for all infrastructure resource names"
+default = "monitor-app"
+}
 
+var "environment" {
+type = string
+description = "The deployment stage(e.g., dev, staging, prod)"
+default = "dev"
+}
+
+# 2. Cloud Provider Configurations
+variable = "aws_region" {
+type = string
+description = "The deployment stage(e.g., dev, staging, prod)"
+default = "dev"
+}
+
+# 3. Compute / Instance Configurations
+variable = "instance_type" {
+type = string
+description = " "
+default = " "
