54960eed975e4930ea131581968459bab47400f5
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-12 03:39:36 +0100

    Add main function and entry point to main.py

--- sre-monitor-app/main.py
+++ sre-monitor-app/main.py
@@ -1,1 +1,10 @@
+import sys
+from my_package import my_module
 
+def main()-> int:
+  """The main entry point of the program."""
+  print("Applicatiom is running...")
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
