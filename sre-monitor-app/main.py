e419eabd120b09346c70fdba5636937e4ec6f4c7
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-08-16 18:02:22 +0100

    Update main.py

--- sre-monitor-app/main.py
+++ sre-monitor-app/main.py
@@ -1,10 +1,28 @@
 import sys
+from flask import Flask, render_template, jsonify
+import requests
 from my_package import my_module
 
-def main()-> int:
-  """The main entry point of the program."""
-  print("Applicatiom is running...")
-  return 0
+app = Flask(__name__)
 
-if __name__ == "__main__":
-  sys.exit(main())
+JAVA_BACKEND_URL = "https://onreader.com"
+
+@app.route('/')
+def home():
+   "Renders the dashboard with live data from the Java microservice""".
+  try:
+    response =  requests.get(JAVABACKEND URL, time=6)
+   system_data = {"status": "Connected" "uptime": "N/A"}
+
+
+return render_template('index.html', system_data=system_data)
+
+
+def main() -> int:
+"""The main entry point to start the application locally."""
+print ("Application is running on http://127.0.0.1:5000...")
+app.run(host=0.0.0.0, port=5000, debug=True)
+return 0
+
+if __name__ = "__main__"
+       sys.exit(main())
