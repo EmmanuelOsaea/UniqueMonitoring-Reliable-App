59b2b88fab94ab2b13d5702b7bd9ed05c5c6bb28
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-09-02 20:11:12 +0100

    Create BackendApplication.java

---
+++ sre-monitor-app/backend-java/BackendApplication.java
@@ -0,0 +1,12 @@
+package com.monitoring.backend;
+
+import org springframework.boot.SpringApplication;
+import org springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+ Public class BackendApplication {
+
+public static void main (String[] args) {
+  SpringApplication.run(BackendApplication.class, args)
+    }
+}
